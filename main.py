5cdf6f59bf472df35ffd228f86cc4d96fe01b5d4
timchenkoMP <timchenko.margari@yandex.ru>
2026-05-05 21:31:55 +0300

    feat: add subtract function

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ def add(a, b):
     """Возвращает сумму двух чисел."""
     return a + b
 
+def subtract(a, b):
+    """Возвращает разность двух чисел."""
+    return a - b
+
 def main():
     print("=== Калькулятор ===")
     print(f"2 + 3 = {add(2, 3)}")
+    print(f"5 - 2 = {subtract(5, 2)}")
 
 if __name__ == "__main__":
     main()
